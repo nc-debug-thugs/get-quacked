1b985923093bed050085e13fe764ebe56eb8bbfd
Harry Fleming <email@harryfleming.net>
2023-04-28 12:32:12 +0100

    implement base bullet class

---
+++ src/classes/BaseBullet.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser'
+
+export default class BaseBullet extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, imageKey) {
+    super(scene, 0, 0, imageKey)
+    this.gameWidth = this.scene.scale.gameSize.width
+    this.gameHeight = this.scene.scale.gameSize.height
+  }
+  
+  fire(originX, originY, angle, angleOffset, speed) {
+    this.setPosition(originX, originY)
+    const vector = this.scene.physics.velocityFromAngle(angle + angleOffset, speed)
+    this.setVelocity(vector.x, vector.y)
+    this.setAngle(angle)
+    this.setActive(true)
+    this.setVisible(true)
+  }
+
+  update() {
+    if (this.x < 0 || this.x > this.gameWidth || this.y < 0 || this.y > this.gameHeight) {
+      this.destroy()
+    }
+  }
+}
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,5 +1,12 @@
 import Phaser from 'phaser'
 import Player from '../classes/Player'
+import BaseBullet from '../classes/BaseBullet'
+
+class PlayerBullet extends BaseBullet {
+  constructor(scene) {
+    super(scene, 'bullet')
+  }
+}
 
 export default class Play extends Phaser.Scene {
   constructor() {
@@ -11,7 +18,12 @@ export default class Play extends Phaser.Scene {
   create() {
     this.cursors = this.input.keyboard.createCursorKeys()
 
-    this.playerBulletGroup = this.physics.add.group({})
+    this.playerBulletGroup = this.physics.add.group({
+      classType: PlayerBullet,
+      maxSize: 1,
+      runChildUpdate: true
+    })
+
     this.player = new Player(this, 400, 300, 'duck', this.playerBulletGroup)
     this.add.existing(this.player)
   }

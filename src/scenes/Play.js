1b87f975481fd55309fc0c0f3ae470aa603913c0
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-05-02 10:50:10 +0100

    feat: destroy enemy when hit by player's bullet

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -39,14 +39,12 @@ export default class Play extends Phaser.Scene {
     this.path = new Phaser.Curves.Path();
     this.path.add(new Phaser.Curves.Ellipse(400, 300, 265));
 
-    this.hunters = this.add.group({});
+    this.hunters = this.physics.add.group({});
     this.hunter1 = new Hunter(this, this.path, 0, 0);
     this.hunter2 = new Hunter(this, this.path, 0, 0);
     this.hunter3 = new Hunter(this, this.path, 0, 0);
 
     this.hunters.addMultiple([this.hunter1, this.hunter2, this.hunter3]);
-
-    console.log(this.hunters);
 
     this.hunters.getChildren().forEach((hunter, i, hunters) => {
       this.add.existing(hunter);
@@ -60,13 +58,19 @@ export default class Play extends Phaser.Scene {
       );
     });
 
-    // this.hunter = new Hunter(this, this.path, 0, 0);
-    // this.add.existing(this.hunter);
-    // this.hunters.startFollow({
-    //   duration: 9000,
-    //   repeat: -1,
-    //   rotateToPath: true,
-    // });
+    // player bullet and hunter interaction
+    this.physics.add.overlap(
+      this.playerBulletGroup,
+      this.hunters,
+      this.handleEnemyHit,
+      null,
+      this
+    );
+  }
+
+  handleEnemyHit(playerBullet, hunter) {
+    playerBullet.destroy();
+    hunter.destroy();
   }
 
   update() {

d257d331a822ff5e3ad37d0c7c6200dd68ed91e8
lynhodinh <lhodinh@gmail.com>
2023-05-04 12:33:03 +0100

    added start button animation

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -22,6 +22,12 @@ export default class Start extends Phaser.Scene {
     );
     startButton.setScale(0.5);
     startButton.setInteractive();
+    startButton.on("pointerover", () => {
+      this.tweens.add({
+        targets: startButton,
+        scale: { value: 0.6, duration: 100, yoyo: true, ease: "Power1" },
+      });
+    });
     startButton.on("pointerdown", () => {
       this.scene.start("play");
     });

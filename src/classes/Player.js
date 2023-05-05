0d01ce6fccca1cd4386e6a1ab0758e8adec6d49f
Zipeth <markchristensen1010@gmail.com>
2023-05-05 14:02:58 +0100

    final changes added

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -6,42 +6,42 @@ export default class Player extends Phaser.GameObjects.Sprite {
     this.setActive(true);
     this.setVisible(true);
     this.setDepth(2);
-    this.setScale(.7);
+    this.setScale(0.7);
 
     this.bulletGroup = bulletGroup;
     this.isVulnerable = true;
-    this.canShoot = true
+    this.canShoot = true;
 
     scene.anims.create({
-      key: 'player-invulnerable',
-      frames: this.anims.generateFrameNumbers('duck', {
+      key: "player-invulnerable",
+      frames: this.anims.generateFrameNumbers("duck", {
         start: 0,
-        end: 1
+        end: 1,
       }),
       frameRate: 24,
-      repeat: -1
-    })
+      repeat: -1,
+    });
 
     scene.anims.create({
-      key: 'player-vulnerable',
-      frames: this.anims.generateFrameNumbers('duck', {
+      key: "player-vulnerable",
+      frames: this.anims.generateFrameNumbers("duck", {
         start: 0,
-        end: 0
-      }),
-      frameRate: 24,
-      repeat: 0
-    })
-
-    scene.anims.create({
-      key: 'player-dies',
-      frames: this.anims.generateFrameNumbers('boom', {
-        start: 0,
-        end: 23
+        end: 0,
       }),
       frameRate: 24,
       repeat: 0,
-      hideOnComplete: true
-    })
+    });
+
+    scene.anims.create({
+      key: "player-dies",
+      frames: this.anims.generateFrameNumbers("boom", {
+        start: 0,
+        end: 23,
+      }),
+      frameRate: 24,
+      repeat: 0,
+      hideOnComplete: true,
+    });
   }
 
   shoot() {
@@ -49,14 +49,14 @@ export default class Player extends Phaser.GameObjects.Sprite {
       const bullet = this.bulletGroup.get();
       if (bullet) {
         bullet.fire(this.x, this.y, this.angle, 0, 90, 600);
-        this.canShoot = false
+        this.canShoot = false;
         this.scene.time.addEvent({
           delay: 500,
           loop: false,
           callback: () => {
-            this.canShoot = true
-          }
-        })
+            this.canShoot = true;
+          },
+        });
       }
     }
   }
@@ -64,26 +64,26 @@ export default class Player extends Phaser.GameObjects.Sprite {
   hit(health) {
     if (this.isVulnerable) {
       this.isVulnerable = false;
-      this.play('player-invulnerable')
+      this.play("player-invulnerable");
       this.scene.time.addEvent({
         delay: 2000,
         loop: false,
         callback: () => {
           if (this) {
             this.isVulnerable = true;
-            this.play('player-vulnerable')
+            this.play("player-vulnerable");
           }
         },
       });
       const isDead = health.decreaseHealth();
       if (isDead) {
-        this.die()
+        this.die();
       }
-      return isDead
+      return isDead;
     }
   }
 
   die() {
-    this.play('player-dies', true)
+    this.play("player-dies", true);
   }
 }
